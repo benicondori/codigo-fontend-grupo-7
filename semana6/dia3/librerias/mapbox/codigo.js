7e0552684b9e866823d4ea4f4ec82829cca306d8
Your Name <you@example.com>
2021-04-01 23:27:34 -0500

    usando marcador y mensaje

--- semana6/dia3/librerias/mapbox/codigo.js
+++ semana6/dia3/librerias/mapbox/codigo.js
@@ -1,7 +1,13 @@
 mapboxgl.accessToken = "pk.eyJ1IjoiYmVuaWNvbmRvcmkiLCJhIjoiY2ttem8xd2NoMGNnMzJvbzJubHNueXQxZiJ9.hunL3t4LXRN-uB_lY-i2qg";
 let map = new mapboxgl.Map({
 container: "mapa", // container ID
-style: "mapbox://styles/mapbox/streets-v11", // style URL
-center: [-13.52264, -71.96734], // starting position [lng, lat]
-zoom: 2 // starting zoom
+style: "mapbox://styles/mapbox/streets-v9", // style URL
+//center: [-13.52264, -71.96734], // starting position [lng, lat]
+zoom: 1 // starting zoom
 });
+let mensaje=new mapboxgl.Popup()
+.setHTML("<h3>buenos dias</h3><p>empecemos..!!</p>");
+let marcador=new mapboxgl.Marker()
+.setLngLat([-13.52264, -71.96734])
+.setPopup(mensaje)
+.addTo(map);
